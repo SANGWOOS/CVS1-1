976a80ed855957f7ffa17e862b41ea090bc0389d
input0 <jason9605@naver.com>
2022-09-27 13:22:24 +0900

    Add files via upload

---
+++ AndroidFiles/CSV11/app/src/main/java/com/example/cvs11/DataModel.java
@@ -0,0 +1,61 @@
+package com.example.cvs11;
+
+public class DataModel {
+    String brand;
+    String type;
+    Information[] prod_list;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Information[] getProd_list() {
+        return prod_list;
+    }
+
+    public void setProd_list(Information[] prod_list) {
+        this.prod_list = prod_list;
+    }
+
+    static public class Information {
+        String name;
+        String price;
+        String image;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public void setImage(String image) {
+            this.image = image;
+        }
+    }
+}
