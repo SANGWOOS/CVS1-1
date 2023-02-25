e7874402c69cf18f25fb180a0577cf8f7d5d629e
SANGWOOS <64746848+SANGWOOS@users.noreply.github.com>
2023-02-26 01:11:25 +0900

    Add files via upload

---
+++ AndroidFiles/CVS11/app/src/main/java/com/example/cvs11/item_info.java
@@ -0,0 +1,58 @@
+package com.example.cvs11;
+
+public class item_info {
+    String imageURL;
+    String tag;
+    String brand;
+    String name;
+    String price;
+    String PID;
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPID() {
+        return PID;
+    }
+
+    public void setPID(String PID) {
+        this.PID = PID;
+    }
+}
